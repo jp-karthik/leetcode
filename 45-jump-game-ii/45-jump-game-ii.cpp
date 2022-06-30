3434ff7040ab5a8b94724184c188f6b3a9261752
Karthik JP <karthikjagadeeshponarkar@gmail.com>
2022-06-30 16:03:28 +0530

    Time: 1080 ms (6.71%), Space: 18.4 MB (6.06%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -4,18 +4,6 @@ public:
         int n = nums.size();
         vector<pair<bool, long long>> dp(n, {false, 1e10});  
         dp[0] = {true, 0};
-        // for (int i = 1; i < n; i++) {
-        //     for (int k = i - 1; k >= 0; k--) {
-        //         if (dp[k].first) {
-        //             int req = i - k;
-        //             if (nums[k] >= req) {
-        //                 dp[i].second = min(dp[i].second, dp[k].second + 1);
-        //                 dp[i].first = true;
-        //             }
-        //         }
-        //     }
-        // }
-        
         for (int i = 0; i < n; i++) {
             if (dp[i].first) {
                 for (int j = 1; j <= nums[i]; j++) {
