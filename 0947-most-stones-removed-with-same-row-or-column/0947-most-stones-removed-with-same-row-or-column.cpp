eab77a86927f64f89bc0e40cc3dcfba7af3378b7
Karthik JP <karthikjagadeeshponarkar@gmail.com>
2022-11-14 20:51:48 +0530

    Time: 590 ms (17.12%), Space: 16.7 MB (51.22%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> g;
+    vector<bool> vis;
+    
+    int dfs(int u) {
+        vis[u] = true;
+        int curr = 0;
+        for (auto v : g[u]) {
+            if (!vis[v]) {
+                curr += dfs(v);
+            }
+        }
+        return curr + 1;
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        
+        // nodes : 0 to n - 1
+        g.resize(n, {});
+        vis.resize(n, false);
+        
+        // creating graph
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                bool b = stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1];
+                if (b) {
+                    g[i].push_back(j);
+                    g[j].push_back(i);
+                }
+            }
+        }
+        
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                res += dfs(i) - 1;
+            }
+        }
+        
+        g.clear();
+        vis.clear();
+        
+        return res;
+    }
+};
